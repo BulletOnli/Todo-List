89fd7dd0572b494a25a370f5e03db66785742ca0
Gemmuel <gemmueldelacruz@gmail.com>
2022-09-20 23:00:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+let todos = []
+
+function addTodo() {
+    const inputTodo = document.getElementById('inputTodo');
+    const title = inputTodo.value;
+    const id = '' + new Date().getTime();
+
+    todos.push({
+        name: title,
+        id: id
+    })
+    inputTodo.value = '';
+    console.log(todos)
+    showTodo()
+}
+
+function removeTodo(event) {
+    const deleteBtn = event.target
+    const idToDelete = deleteBtn.id
+
+    todos = todos.filter(todo => todo.id !== idToDelete)
+    showTodo()
+    console.log(todos)
+}
+
+function showTodo() {
+    document.getElementById("todoContainer").textContent = '';
+    // document.getElementById("deleteBtnContainer").textContent = '';
+
+    todos.forEach(function(todo) {
+        const titleContainer = document.createElement('p');
+        titleContainer.id = "titleContainer"
+        titleContainer.textContent = "● " + todo.name;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Delete"
+        deleteBtn.style.padding = "5px 10px"
+        deleteBtn.style.borderRadius = "5px"
+        deleteBtn.style.border = "1px solid black"
+        deleteBtn.style.marginLeft = "5px";
+        deleteBtn.style.backgroundColor = "#ffb5a7"
+        deleteBtn.style.cursor = "pointer"
+        deleteBtn.id = todo.id;
+        deleteBtn.onclick = removeTodo;
+        titleContainer.appendChild(deleteBtn)
+
+        const todoContainer = document.getElementById('todoContainer');
+        todoContainer.appendChild(titleContainer)
+    })
+}
